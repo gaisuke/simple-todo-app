dd8c03a6fdaed1c6ca25c980a3d23f682139fa9c
gaisuke <danimunif@cs.unej.ac.id>
2023-09-13 20:00:07 +0700

    feat(script): create a delete button and it's function

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ document.querySelector('#create-todo-btn').onclick = function(){
         alert('Please input your todo!')
     } else {
         document.querySelector('#view-todo').innerHTML +=`
-        <div class="todo-item">
+        <div class="todo-item" style="display: flex; justify-content: space-around">
             <p id="todo-text">
                 ${document.querySelector('#create-todo input').value}
             </p>
+            <button class="delete-btn">
+            X
+            </button>
         </div>
         `
+        var currentTodo = document.querySelectorAll('.delete-btn');
+        for (let i = 0; i < currentTodo.length; i++) {
+            currentTodo[i].onclick = function() {
+                this.parentNode.remove();
+            }
+        }
     }
 }
